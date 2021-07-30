1a00a8efccdbb8fb4e8d0d268dff84bb5b1e5419
Kartik Agarwal <55729594+kartik-29@users.noreply.github.com>
2021-07-30 12:04:23 +0530

    refactor: dev setup

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 import express from 'express';
 import  dotenv from 'dotenv';
 import morgan from 'morgan';
-import mongoDb from './db.js';
-import router from './router.js';
-import bodyParser from 'body-parser';
+import routes from './routes/index.js';
+
 const app = express();
 
 const errorHandler = (req, res, next) => console.log(`ERROR: URL NOT found`);
 dotenv.config();
 
-// await mongoDb.connect(process.env.MONGODB_URL, process.env.MONGODB_NAME);
-
 app.use(morgan('dev'));
-app.use('/api', router);
-app.use(bodyParser.json({ limit: '20mb' }))
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
+app.use('/api', routes);
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, (error) => {
     if (error) {
@@ -23,6 +20,4 @@ app.listen(process.env.PORT, (error) => {
     } else {
         console.log(`server listening on PORT ${process.env.PORT}`);
     }
-})
-
-app.use(errorHandler);
+});
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.use(express.json( { limit: '20mb' } ));
+router.use(express.urlencoded({ limit: '20mb', extended: false }));
+
+router.get('/health', (req, res, next) => res.send("Up & running"));
+// segregation of routes as per the modules here.
+
+export default router;
