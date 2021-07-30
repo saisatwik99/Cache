20a7670e1b11e17677ad89fcaf191933743da52a
Sai Satwik Kokanti <56591458+saisatwik99@users.noreply.github.com>
2021-07-30 13:09:37 +0530

    feat: added linter (#4)
    * feat: added linter
    
    * fix: removed typescript
    
    * refactor: lint changes
    
    Co-authored-by: Kartik Agarwal <55729594+kartik-29@users.noreply.github.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'max-len': ['error', { code: 120 }],
+    indent: ['error', 2],
+    'comma-dangle': ['error', 'never'],
+    'linebreak-style': 0,
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'no-console': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages'
+    ]
+  }
+};
