8f97b65d30590a3894d28d294220bf637dd04189
Kartik Agarwal <55729594+kartik-29@users.noreply.github.com>
2021-08-05 22:19:35 +0530

    feat; validate token API

--- utils/utils.js
+++ utils/utils.js
@@ -14,25 +14,29 @@ const base64toString = (b64) => Buffer.from(b64, 'base64').toString();
 
 const getToken = (id) => jwt.sign({ id }, envSecret(), jwtConst);
 
+const isTokenValid = (req, res, next) => {
+    const token = (req.headers.authorization && req.headers.authorization.split(' ')[1]) || '';
+    return jwt.verify(token, envSecret(), jwtConst);
+};
+
 const verifyAuthToken = async (req, res, next) => {
   const token = (req.headers.authorization && req.headers.authorization.split(' ')[1]) || '';
-  // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMDY0ZjgzZmRhZjNjZDFlNjZmOGI1NyIsImlhdCI6MTYyNzk4NTM0NywiZXhwIjoxNjI3OTg2NTQ3LCJpc3MiOiJmYW50YSJ9.JgwUMYEBa941q_4FyAi9rNOUcbspT3obIMWHHv0j_1Y';
-  console.log(token);
-  try{
+  try {
     const isVerified = jwt.verify(token, envSecret(), jwtConst);
-  }catch(err){
+
+    const data = jwt.decode(token, jwtConst);
+    const user = await userDB.getUserDetails({ userId: data.id });
+    req.user = user;
+
+    return next();
+  } catch (err) {
     return next(new InvalidJwtError('Invalid Token'));
   }
-    
-  const data = jwt.decode(token, jwtConst);
-  const user = await userDB.getUserDetails({ userId: data.id });
-  req.user = user;
-
-  return next();
-}
+};
 
 export default {
   base64toString,
   getToken,
-  verifyAuthToken
+  verifyAuthToken,
+  isTokenValid
 };
